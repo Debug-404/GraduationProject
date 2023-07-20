8bc50aa559981fb5daf5ac9687366bbd8bdc7ebd
Debug <1944282957@qq.com>
2023-07-20 22:01:16 +0800

    Study

--- src/test/java/com/SpringBootVueApplicationTests.java
+++ src/test/java/com/SpringBootVueApplicationTests.java
@@ -1,14 +1,25 @@
 package com;
 
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.utils.JwtUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class SpringBootVueApplicationTests {
 
     @Test
     public void contextLoads() {
-
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "digua");
+        String token = JwtUtils.createToken(map);
+        DecodedJWT decodedJWT = JwtUtils.verifyToken(token);
+        Map<String, Claim> claims = decodedJWT.getClaims();
+        System.out.println(claims.get("name"));
     }
 
 
